9431b7b825bb5c9b94ec68146b80580464a06734
omer evron <omerevron011@gmail.com>
2022-12-17 16:30:03 +0200

    frontend connection

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ import express, {json} from "express";
 import cors from "cors"
 import './server/db/mongoose.js'
 import { indexBank } from "./server/routes/routes.js";
-
+import * as url from 'url';
+import path from 'path';
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(json());
 app.use(cors());
-
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
 app.use("/api", indexBank);
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+ });
 
+ 
 app.listen(PORT, () => {
    console.log(` app listening on port ${PORT}`);
 });
