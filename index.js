f3ecaed813df3d0553a74d146277406a3da7219d
omer evron <omerevron011@gmail.com>
2022-12-17 21:04:20 +0200

    frontend connection fixed

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ import './server/db/mongoose.js'
 import { indexBank } from "./server/routes/routes.js";
 import * as url from 'url';
 import path from 'path';
-const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+console.log(__dirname);
 const app = express();
 const PORT = process.env.PORT || 8000;
-
 app.use(json());
 app.use(cors());
-const publicPath = path.join(__dirname, 'build');
+const publicPath = path.join(__dirname, 'server','build');
 app.use(express.static(publicPath));
 app.use("/api", indexBank);
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
- });
+   res.sendFile(path.resolve(__dirname, 'server', 'index.html'));
+});
 
- 
+
 app.listen(PORT, () => {
    console.log(` app listening on port ${PORT}`);
 });
