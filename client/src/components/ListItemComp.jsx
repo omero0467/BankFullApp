625505a40f18666ba6e0599804b2ce4c127c3fcb
omer evron <omerevron011@gmail.com>
2022-12-18 12:42:14 +0200

    fixed deploy

--- client/src/components/ListItemComp.jsx
+++ client/src/components/ListItemComp.jsx
@@ -1,55 +1,59 @@
-import axios from "axios";
-import {Spinner} from "flowbite-react";
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-function ListItemComp({user, source}) {
-   const [isLoading, setisLoading] = useState(false);
-   const [balance, setBalance] = useState("");
-   useEffect(() => {
-      async function getBalance() {
-         setisLoading(true);
-         const {data} = await axios.get(
-            `https://bashobankapp.onrender.com/api/accounts/${user.accounts[0]}`
-         );
-         setBalance(data.cash);
-         setisLoading(false);
+import { Spinner } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Api } from "../api/Api";
+function ListItemComp({ user, source }) {
+  const [isLoading, setisLoading] = useState(false);
+  const [balance, setBalance] = useState("");
+  useEffect(() => {
+    if (user.accounts.length > 0) {
+      getBalance();
+    } else {
+      setBalance("No Accounts");
+    }
+    async function getBalance() {
+      try {
+        setisLoading(true);
+        const { data } = await Api.get(`/accounts/${user.accounts[0]}`);
+        setBalance(data.cash);
+        setisLoading(false);
+      } catch (error) {
+        console.log(error);
+        setisLoading(false);
+        setBalance("No Info");
       }
-      if (user.accounts.length > 0) {
-         getBalance();
-      } else {
-         setBalance("No Accounts");
-      }
-   }, [user]);
-   return (
-      <>
-         <li className="py-3 sm:py-4">
-            <div className="flex items-center space-x-4">
-               <div className="shrink-0">
-                  <img
-                     className="h-8 w-8 rounded-full"
-                     src={source}
-                     alt={user.name}
-                  />
-               </div>
-               <div className="min-w-0 flex-1">
-                  <Link
-                     to={`/user/${user._id}`}
-                     className="truncate text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                     {user.name} {user.lastName}
-                  </Link>
-                  <p className="truncate text-sm text-gray-500 dark:text-gray-400">
-                     User ID: {user._id}
-                  </p>
-               </div>
-               <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                  {balance !== "No Accounts" ? `$${balance}` : balance}
-                  {isLoading && <Spinner />}
-               </div>
-            </div>
-         </li>
-      </>
-   );
+    }
+  }, [user]);
+  return (
+    <>
+      <li className="py-3 sm:py-4">
+        <div className="flex items-center space-x-4">
+          <div className="shrink-0">
+            <img
+              className="h-8 w-8 rounded-full"
+              src={source}
+              alt={user.name}
+            />
+          </div>
+          <div className="min-w-0 flex-1">
+            <Link
+              to={`/user/${user._id}`}
+              className="truncate text-sm font-medium text-gray-900 dark:text-white"
+            >
+              {user.name} {user.lastName}
+            </Link>
+            <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+              User ID: {user._id}
+            </p>
+          </div>
+          <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+            {balance !== "No Accounts" ? `$${balance}` : balance}
+            {isLoading && <Spinner />}
+          </div>
+        </div>
+      </li>
+    </>
+  );
 }
 
 export default ListItemComp;
--- index.js
+++ index.js
@@ -5,22 +5,17 @@ import { indexBank } from "./server/routes/routes.js";
 import * as url from 'url';
 import path from 'path';
 const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
-​
 const app = express();
 const PORT = process.env.PORT || 8000;
-​
 app.use(json());
 app.use(cors());
 const publicPath = path.join(__dirname, 'server','build');
 app.use(express.static(publicPath));
 app.use("/api", indexBank);
-​
 app.get('*', (req, res) => {
    console.log(__dirname);
    res.sendFile(path.resolve(__dirname, 'server','build','index.html'));
  });
-​
- 
 app.listen(PORT, () => {
    console.log(` app listening on port ${PORT}`);
 });
